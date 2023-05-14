c0a833f9be57bf80448ac1c128711cc012d6ca49
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-05-14 08:38:52 +0530

    style: navbar and hero image style added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,28 @@
 import "./Header.css";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PersonIcon from "@mui/icons-material/Person";
 
 export const Header = () => {
   return (
     <div className="header">
       <div className="header_heading">
-        <h2>BooksMedia</h2>
+        <h2>BOOKSMEDIA</h2>
       </div>
       <div className="header_action">
-        <p className="login">Login</p>
-        <p className="wishlist">Wishlist</p>
-        <p className="cart">Cart</p>
+        <div className="wishlist">
+          <FavoriteOutlinedIcon />
+        </div>
+        <div className="cart">
+          <ShoppingCartIcon />
+        </div>
+        <div className="user">
+          <PersonIcon />
+        </div>
       </div>
       <div className="header_search">
+        <SearchOutlinedIcon />
         <input />
       </div>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,9 @@
+import "./Home.css";
+
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <div className="home">
+      <button className="shop-btn">SHOP NOW</button>
+    </div>
+  );
 };
