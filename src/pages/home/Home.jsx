f2e7fa86b432d548435d1f2a628e58bc43ab22a3
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-05-23 00:21:49 +0530

    added category filter on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import "./Home.css";
-import { NavLink } from "react-router-dom";
-import { useCategories } from "../../index";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useBooks, useCategories } from "../../index";
+import { filterTypes } from "../../constants/FilterTypes";
+import { useEffect } from "react";
 
 export const Home = () => {
   const { categories } = useCategories();
+  const {
+    booksState: { books },
+    booksDispatch,
+  } = useBooks();
+  const navigate = useNavigate();
+  const { CATEGORY_FILTER, CLEAR_FILTER } = filterTypes;
+
+  useEffect(() => {
+    booksDispatch({ type: CLEAR_FILTER, payload: books });
+  }, []);
 
   return (
     <div className="home">
@@ -15,13 +27,20 @@ export const Home = () => {
       <h1>Browse by Category</h1>
       <ul className="home_wrapper">
         {categories?.map(({ _id, categoryName }) => (
-          <NavLink
+          <button
             key={_id}
-            to={`/books/${categoryName}`}
             className="home_wrapper_item"
+            onClick={() => {
+              booksDispatch({
+                type: CATEGORY_FILTER,
+                payload: categoryName,
+              });
+              navigate("/books");
+            }}
           >
             <p>{categoryName}</p>
-          </NavLink>
+            <p>|</p>
+          </button>
         ))}
       </ul>
     </div>
