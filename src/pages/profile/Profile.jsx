94f4f2eb0cf2bfb634810fd1329d0906d6307619
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-06 18:33:25 +0530

    fix: profile route bug fixed

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,25 +1,34 @@
 import "./Profile.css";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 export const Profile = () => {
-  const headings = ["Profile", "Address"];
-  const routes = ["", "address"];
+  const location = useLocation();
 
   return (
     <div className="profile_page">
       <ul className="profile_list">
-        {headings?.map((heading, index) => (
-          <li className="profile_list_item" key={index}>
-            <NavLink
-              to={`/profile/${routes[index]}`}
-              className={({ isActive }) =>
-                isActive ? "navlink active_navlink" : "navlink"
-              }
-            >
-              <h2>{heading}</h2>
-            </NavLink>
-          </li>
-        ))}
+        <li className="profile_list_item">
+          <NavLink
+            to="/profile"
+            className={
+              location.pathname === "/profile"
+                ? "navlink active_navlink"
+                : "navlink"
+            }
+          >
+            <h2>Profile</h2>
+          </NavLink>
+        </li>
+        <li className="profile_list_item">
+          <NavLink
+            to="/profile/address"
+            className={({ isActive }) =>
+              isActive ? "navlink active_navlink" : "navlink"
+            }
+          >
+            <h2>Address</h2>
+          </NavLink>
+        </li>
       </ul>
       <hr />
       <Outlet />
